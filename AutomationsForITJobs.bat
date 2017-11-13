9c328efcf66e33e9e96f89a469b544845c4a74c2
Ricardo Kim <hexadecimal.exe@gmail.com>
2017-11-12 23:42:18 -0200

    Create AutomationsForITJobs.bat

---
+++ AutomationsForITJobs.bat
@@ -0,0 +1,585 @@
+@echo off
+color A
+
+:SENHA
+	@echo.   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	@echo. *								*
+	@echo. *		Plataforma de Reparo e Configura‡äes		*
+	@echo. *								*
+	@echo. * 	Desenvolvimento: 	Ricardo Kim			*
+	@echo. *	Contato:		hexadecimal.exe@gmail.com	*
+	@echo. *					v7.0			*
+	@echo. *								*
+	@echo.   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	echo.
+	echo.
+	echo 	dica: tudo junto
+	echo 	Sequencia:[ 2, ..., 5, 7, ..., 13, 17, ..., 23, ..., 31, 37 ]
+	echo 	ascii: 174-175
+	echo.
+	set /P type=Senha de Acesso:  
+	IF /I %type% EQU 3111929®¯ (GOTO MENU
+		) ELSE ( GOTO TRAP
+		)
+
+:TRAP
+	cls
+	echo .##....##.########..######......###....########...#######.
+	echo .###...##.##.......##....##....##.##...##.....##.##.....##
+	echo .####..##.##.......##.........##...##..##.....##.##.....##
+	echo .##.##.##.######...##...####.##.....##.##.....##.##.....##
+	echo .##..####.##.......##....##..#########.##.....##.##.....##
+	echo .##...###.##.......##....##..##.....##.##.....##.##.....##
+	echo .##....##.########..######...##.....##.########...#######.
+	echo.
+	echo .##....##.########..######......###....########...#######.
+	echo .###...##.##.......##....##....##.##...##.....##.##.....##
+	echo .####..##.##.......##.........##...##..##.....##.##.....##
+	echo .##.##.##.######...##...####.##.....##.##.....##.##.....##
+	echo .##..####.##.......##....##..#########.##.....##.##.....##
+	echo .##...###.##.......##....##..##.....##.##.....##.##.....##
+	echo .##....##.########..######...##.....##.########...#######.
+	echo.
+	echo .##....##.########..######......###....########...#######.
+	echo .###...##.##.......##....##....##.##...##.....##.##.....##
+	echo .####..##.##.......##.........##...##..##.....##.##.....##
+	echo .##.##.##.######...##...####.##.....##.##.....##.##.....##
+	echo .##..####.##.......##....##..#########.##.....##.##.....##
+	echo .##...###.##.......##....##..##.....##.##.....##.##.....##
+	echo .##....##.########..######...##.....##.########...#######.
+	echo.
+	echo .##....##.########..######......###....########...#######.
+	echo .###...##.##.......##....##....##.##...##.....##.##.....##
+	echo .####..##.##.......##.........##...##..##.....##.##.....##
+	echo .##.##.##.######...##...####.##.....##.##.....##.##.....##
+	echo .##..####.##.......##....##..#########.##.....##.##.....##
+	echo .##...###.##.......##....##..##.....##.##.....##.##.....##
+	echo .##....##.########..######...##.....##.########...#######.
+	echo.
+	echo .##....##.########..######......###....########...#######.
+	echo .###...##.##.......##....##....##.##...##.....##.##.....##
+	echo .####..##.##.......##.........##...##..##.....##.##.....##
+	echo .##.##.##.######...##...####.##.....##.##.....##.##.....##
+	echo .##..####.##.......##....##..#########.##.....##.##.....##
+	echo .##...###.##.......##....##..##.....##.##.....##.##.....##
+	echo .##....##.########..######...##.....##.########...#######.
+	echo.
+	echo .##....##.########..######......###....########...#######.
+	echo .###...##.##.......##....##....##.##...##.....##.##.....##
+	echo .####..##.##.......##.........##...##..##.....##.##.....##
+	echo .##.##.##.######...##...####.##.....##.##.....##.##.....##
+	echo .##..####.##.......##....##..#########.##.....##.##.....##
+	echo .##...###.##.......##....##..##.....##.##.....##.##.....##
+	echo .##....##.########..######...##.....##.########...#######.
+	echo.
+	echo .##....##.########..######......###....########...#######.
+	echo .###...##.##.......##....##....##.##...##.....##.##.....##
+	echo .####..##.##.......##.........##...##..##.....##.##.....##
+	echo .##.##.##.######...##...####.##.....##.##.....##.##.....##
+	echo .##..####.##.......##....##..#########.##.....##.##.....##
+	echo .##...###.##.......##....##..##.....##.##.....##.##.....##
+	echo .##....##.########..######...##.....##.########...#######.
+
+:C1
+	color 1
+	goto C2
+
+:C2
+	color 2
+	goto C3
+
+:C3
+	color 3
+	goto C4
+
+:C4
+	color 4
+	goto C5
+
+:C5
+	color 5
+	goto C6
+
+:C6
+	color 6
+	goto C7
+
+
+:C7
+	color 7
+	goto C8
+
+:C8
+	color 8
+	goto C9
+
+:C9
+	color 9
+	goto CA
+
+:CA
+	color A
+	goto CB
+
+:CB
+	color B
+	goto CC
+
+:CC
+	color C
+	goto CD
+
+:CD
+	color D
+	goto CE
+
+:CE
+	color e
+	goto CF
+
+:CF
+	color f
+	GOTO C1
+
+
+:MENU
+	cls
+	@echo.   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	@echo. *								*
+	@echo. *		Plataforma de Reparo e Configura‡äes		*
+	@echo. *								*
+	@echo. * 	Desenvolvimento: 	Ricardo Kim			*
+	@echo. *	Contato:		hexadecimal.exe@gmail.com	*
+	@echo. *					v7.0			*
+	@echo. *								*
+	@echo.   * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	set nxt=0
+	set ric=0
+	echo.
+	echo Fun‡äes dispon¡veis?
+	echo.
+	echo 1 = Erros leves no windows.
+	echo.
+	echo 2 = Impressora congestionada.
+	echo.
+	echo 3 = Performace de rendimento.
+	echo.
+	echo 4 = Habilitar NFE Millenium.
+	echo.
+	echo 5 = IP GrupoFashion
+	echo.
+	echo 6 = Cadeado (1)
+	echo.
+	echo 7 = Cadeado (0)
+	echo.
+	echo 8 = Optimizar
+	echo.
+	echo 9 = Sair
+	echo.
+	set /p type=o que deseja fazer?  
+	if %type% == 1 GOTO SCANDISK
+	if %type% == 2 GOTO SPOOLER
+	if %type% == 3 GOTO DEFRAG
+	if %type% == 4 GOTO NF
+	if %type% == 5 GOTO VERWIN
+	if %type% == 6 GOTO BLOCK
+	if %type% == 7 GOTO ALLOW
+	if %type% == 8 GOTO OPTIMIZATION
+	if %type% == 9 GOTO Exit
+
+	
+:LOADING
+	cls
+	echo desculpe op‡Æo indispon¡vel.
+	echo sistema em desenvolvimento aguarde os update
+	echo.
+	pause
+	GOTO MENU
+
+:SCANDISK
+	cls
+	echo em caso de falha tente a segunda alternativa (necessario reparo agendado ao reiniciar)
+	echo.
+	echo 1 = Reparos leves
+	echo.
+	echo 2 = Reparos medianos
+	echo.
+	echo.
+	set /p type=oque deseja fazer?  
+	if %type% == 1 GOTO SCANDISK1
+	if %type% == 2 GOTO SCANDISK2
+	
+:SCANDISK1
+	cls
+	chkdsk
+	echo.
+	echo.
+	echo.
+	echo por favor efetue uma escolha:
+	echo.
+	echo 1 = Primeiro Menu
+	echo.
+	echo 2 = Reparo a n¡vel mediano
+	echo.
+	echo 3 = Resolvido sair do programa
+	echo.
+	echo.
+	set /p type=o que deseja fazer?
+	echo.
+	if %type% == 2 GOTO SCANDISK2
+	if %type% == 1 GOTO MENU
+	if %type% == 3 GOTO Exit
+
+
+:SCANDISK2
+	cls
+	echo aguarde o andamento completo
+	timeout 2
+	chkdsk /f
+	echo.
+	echo.
+	echo no final por favor aperte S e clique em enter
+	echo.
+	echo salve todos os programas antes de confirmar a continuidade (computador será reiniciado)
+	pause
+	shutdown -r -t 0
+
+
+:SPOOLER
+	cls
+	timeout 2
+	net stop spooler 
+	cd %systemroot%\system32\spool\PRINTERS 
+	del /f /s *.shd 
+	del /f /s *.spl
+	net start spooler 
+	echo.
+	echo.
+	echo.
+	GOTO OPTION1
+
+:DEFRAG
+	cls
+	echo qual windows atual?
+	echo.
+	echo 1 = Windows Xp ou inferior
+	echo.
+	echo 2 = Windows Vista ou Superior
+	echo.
+	set /p type=oque deseja fazer?  
+	if %type% == 1 GOTO DEFRAGXP
+	if %type% == 2 GOTO DEFRAGW7
+
+
+:DEFRAGXP
+	cls
+	echo melhora de performace
+	timeout 5
+	defrag C: -f -v
+	defrag D: -f -v
+	defrag E: -f -v
+	echo.
+	GOTO OPTION1
+
+
+:DEFRAGW7
+	cls
+	echo melhora de performace  
+	timeout 5
+	defrag /C /D /U /V
+	echo.
+	echo.
+	echo.
+	GOTO OPTION 1
+
+
+:NF
+	cls
+	echo Selecione uma opcao:
+	echo.
+	echo 1 = Cliente (loja) (unidade C)
+	echo.
+	echo 2 = Cliente (loja) (unidade D)
+	echo.
+	echo 3 = Servidor (Departamento de TI)
+	echo.
+	echo.
+	set /p type=oque deseja fazer?  
+	if %type% == 1 GOTO NF1
+	if %type% == 2 GOTO NF2
+	if %type% == 3 GOTO NF3
+
+
+:NF1
+	cls
+	regsvr32 c:\millenium\msxml5.dll
+	regsvr32 c:\millenium\capicom.dll
+	regsvr32 c:\millenium\midas.dll
+	regsvr32 c:\millenium\nfeemailx.dll
+	echo Confirmar o Registros antes  de prosseguir
+	echo.
+	pause
+	cls
+	GOTO OPTION1
+:NF2
+	cls
+	regsvr32 d:\millenium\msxml5.dll
+	regsvr32 d:\millenium\capicom.dll
+	regsvr32 d:\millenium\midas.dll
+	regsvr32 d:\millenium\nfeemailx.dll
+	echo Confirmar o Registros antes  de prosseguir
+	echo.
+	pause
+	cls
+	GOTO OPTION1
+
+:NF3
+	cls
+	regsvr32 c:\wts\msxml5.dll
+	regsvr32 c:\wts\capicom.dll
+	regsvr32 c:\wts\midas.dll
+	regsvr32 c:\wts\nfeemailx.dll
+	echo Confirmar o Registros antes  de prosseguir
+	echo.
+	pause
+	cls
+	GOTO OPTION1
+
+
+
+:VERWIN
+	cls
+	echo por favor efetue uma escolha:
+	echo 1 = Windows XP
+	echo 2 = Windows Vista ou 7
+	echo 3 = Windows 8
+	echo. VersÆo do Windows?
+	set /p type=  
+	if %type% == 1 GOTO IPWXP
+	if %type% == 2 GOTO IPWV7
+	if %type% == 3 GOTO IPW08
+	:IPWXP
+	set Entry=ip
+	set WinVer="ConexÆo Local"
+	set Dns=dns
+	cls
+	GOTO FILIAL
+	
+:IPWV7
+	set Entry=ipv4
+	set WinVer="ConexÆo Local"
+	set Dns=dnsservers
+	cls
+	GOTO FILIAL
+
+
+:IPW08
+	set Entry=ipv4
+	set WinVer=Ethernet
+	set Dns=dnsservers
+	cls
+	GOTO FILIAL
+
+
+:FILIAL
+	echo Filial Local
+	echo.
+	echo.
+	echo por favor efetue uma escolha:
+	echo 1 = Miller 360 (Daskim)
+	echo 2 = Maria Marcolina (Barateiro Topy)
+	echo 3 = Miller (Dazans)
+	echo 4 = Miller 50 (Big)
+	echo 5 = Lago da Concordia (Topy)
+	echo.
+	set /p type=onde ir?  
+	if %type% == 1 GOTO MILLER360
+	if %type% == 2 GOTO TP
+	if %type% == 3 GOTO DZ
+	if %type% == 4 GOTO BG
+	if %type% == 5 GOTO BT
+
+
+:MILLER360
+	set p2p=10.4.69
+	cls
+	GOTO IP
+
+:TP
+	set p2p=10.4.68
+	cls
+	GOTO IP
+
+:DZ
+	set p2p=10.8.57
+	cls
+	GOTO IP
+
+:BG
+	set p2p=10.4.62
+	cls
+	GOTO IP
+
+:BT
+	set p2p=10.4.70
+	cls
+	GOTO IP
+
+
+:ERRO
+	echo.
+	echo ERRO
+	echo.
+	echo encerrando em
+	echo.
+	timeout 5
+	echo.
+	Exit
+
+
+:IP
+	cls
+	echo Determ¡ne o final do ip
+	echo.
+	echo.
+	set /p final=um valor entre 10 e 250: 
+	echo.
+	netsh interface %Entry% set address name=%WinVer% source=static %p2p%.%final% 255.255.255.0 %p2p%.1 1
+	netsh interface %Entry% set address name=%WinVer% static %p2p%.%final% 255.255.255.0 %p2p%.1 1
+	
+	cls
+	echo Suas configura‡äes atuais:
+	
+	echo 1 = Digitar DNS
+	echo 2 = Autom tico para 177.75.96.90
+	echo 3 = Autom tico (Sem Configura‡äes)
+	echo 4 = NÆo (retorna pro Menu)
+	echo 5 = NÆo (sair)
+	echo 6 = Prosseguir para pr¢ximo passo sem fazer nada (manter em branco ou configura‡Æo anterior)
+	set /p type= Necessario Alterar Dns? 
+	if %type% == 1 GOTO SETDNS1
+	if %type% == 2 GOTO SETDNS2
+	if %type% == 3 GOTO SETDNSATC
+	if %type% == 4 GOTO MENU
+	if %type% == 5 GOTO Exit
+	if %type% == 6 GOTO DNSCHOOSE
+	
+
+:SETDNS1
+	cls
+	set CurrentDNS1=Digite o Dns Primario
+	set CurrentDNS2=Digite o Dns Secundario
+	GOTO DNSSTC
+
+
+:SETDNS2
+	cls
+	set CurrentDNS1=177.75.96.90
+	set CurrentDNS2=177.75.96.91
+	GOTO DNSSTC
+
+
+:DNSSTC
+	cls
+	netsh int %Entry% set %Dns% %WinVer% static %CurrentDNS1%
+	cls
+	netsh int %Entry% add %Dns% %WinVer% %CurrentDNS2%
+	cls
+	GOTO DNSCHOOSE
+
+
+:DNSATC
+	cls
+	netsh int %Entry% set %Dns% %WinVer% dhcp
+	cls
+	GOTO DNSCHOOSE
+	
+:DNSCHOOSE
+	cls
+	echo por favor aguarde 
+	@timeout 3
+	ipconfig
+	echo.
+	echo.
+	echo 1 = Menu
+	echo 2 = Sair
+	echo.
+	set /p type=O que deseja fazer? 
+	if %type% == 1 GOTO MENU
+	if %type% == 2 GOTO Exit
+
+
+:CONREG
+	cls
+	REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "MaxConnectionsPer1_0Server" /t REG_DWORD /d 0x00000012 /f
+	REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v "MaxConnectionsPerServer" /t REG_DWORD /d 0x00000012 /f
+	cls
+	GOTO OPTION
+
+:OPTIMIZATION
+	REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoAutoUpdate /t REG_DWORD /d 0x00000001 /f 
+	REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\Compress old files" /f 
+	REG ADD "HKCU\Control Panel\Keyboard\" /v InitialKeyboardIndicators /t REG_SZ /d 2 /f 
+	REG ADD "HKU\.DEFAULT\Control Panel\Desktop" /v InitialKeyboardIndicators /t REG_SZ /d 2 /f 
+	REG ADD "HKCU\Control Panel\Desktop" /v MenuShowDelay /t REG_SZ /d 100 /f 
+	REG ADD "HKU\.DEFAULT\Control Panel\Desktop" /v MenuShowDelay /t REG_SZ /d 100 /f 
+	REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v DisableThumbnailCache /t REG_SZ /d 1 /f 
+	REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Cache" /v Persistent /t REG_DWORD /d 0x00000000 /f 
+	REG ADD "HKLM\Software\Microsoft\Security Center" /v UpdatesDisableNotify /t REG_DWORD /d 0x00000001 /f 
+	REG ADD "HKLM\System\CurrentControlSet\Control\Terminal Server" /v AllowTSConnections /t REG_DWORD /d 0x00000001 /f 
+	REG ADD "HKLM\System\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0x00000001 /f 
+	REG ADD "HKLM\System\CurrentControlSet\Control\Terminal Server" /v fAllowToGetHelp /t REG_DWORD /d 0x00000001 /f 
+	GOTO OPTION
+
+
+:BLOCK
+	REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoControlPanel /t REG_DWORD /d 0x00000001 /f 
+	REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoDesktopCleanupWizard /t REG_DWORD /d 0x00000001 /f 
+	REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoPropertiesMyComputer /t REG_DWORD /d 0x00000001 /f 
+
+	GOTO OPTION
+	
+
+:ALLOW
+	REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoControlPanel /f
+	REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoDesktopCleanupWizard /f
+	REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoPropertiesMyComputer /f
+
+	GOTO OPTION
+
+
+:OPTION
+	cls
+	echo DIRECIONAR
+	echo.
+	echo.
+	echo 1 = Menu
+	echo.
+	echo 2 = Reiniciar o Computador
+	echo.
+	echo 3 = Sair
+	echo.
+	set /p type=o que deseja fazer?  
+	if %type% == 1 GOTO MENU
+	if %type% == 2 shutdown -r -t 0
+	if %type% == 3 GOTO Exit
+
+
+:OPTION1
+	echo Processo finalizado.
+	echo.
+	echo.
+	echo.
+	echo por favor efetue uma escolha:
+	echo 1 = Voltar ao Inicio
+	echo 2 = Sair
+	set /p type=o que deseja fazer?  
+	if %type% == 1 GOTO MENU
+	if %type% == 2 GOTO Exit
+
+
+:Exit
+	cls
+	echo Obrigado por usar meu sistema...
+	@timeout 2
+	exit
